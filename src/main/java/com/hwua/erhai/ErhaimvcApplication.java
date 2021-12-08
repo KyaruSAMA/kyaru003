7f5b672f1bf16d99db55355d42d17ea3af82b3ed
kyaru <2324470938@qq.com>
2021-12-08 11:16:03 +0800

    1

---
+++ src/main/java/com/hwua/erhai/ErhaimvcApplication.java
@@ -0,0 +1,13 @@
+package com.hwua.erhai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErhaimvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErhaimvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hwua/erhai/ErhaimvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwua.erhai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErhaimvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
